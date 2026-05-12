5eac5cdb17767c0596f5ac7b7d87b773a9237cb1
Luis Rodriguez <cuentadeluis27@gmail.com>
2026-05-11 22:45:59 -0600

    Updates

---
+++ server/assets/_tanstack-start-manifest_v-CkadCL8t.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/corre/OneDrive/Documents/PROYECTOS/sirega-agro-digital/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-Di3mGbiA.js"] }, "/": { filePath: "C:/Users/corre/OneDrive/Documents/PROYECTOS/sirega-agro-digital/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-DufkTVQJ.js"] } }, clientEntry: "/assets/index-Di3mGbiA.js" });
+export {
+  tsrStartManifest
+};
---
+++ server/index.js
@@ -0,0 +1,5 @@
+import { w } from "./assets/worker-entry-B3BXYgwM.js";
+import "node:events";
+export {
+  w as default
+};
